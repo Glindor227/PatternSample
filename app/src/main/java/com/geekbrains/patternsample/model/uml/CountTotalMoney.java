2f87de25afb417c974f797154a4f22b4d4a5a7a0
Андрей <glindor227@gmail.com>
2020-05-04 15:12:11 +0300

    Чуть подчистили

--- app/src/main/java/com/geekbrains/patternsample/model/uml/CountTotalMoney.java
+++ app/src/main/java/com/geekbrains/patternsample/model/uml/CountTotalMoney.java
@@ -3,13 +3,10 @@ package com.geekbrains.patternsample.model.uml;
 import com.geekbrains.patternsample.model.base.StringView;
 
 import java.io.Serializable;
-import java.util.LinkedList;
-import java.util.List;
 
 // покупка по сумме(Пупить яблок на 300руб)
 public class CountTotalMoney implements Serializable,Count<Float>, StringView {
     private Float count;
-    List<String> a;
 
     public CountTotalMoney(Float count) {
         this.count = count;
@@ -17,9 +14,7 @@ public class CountTotalMoney implements Serializable,Count<Float>, StringView {
 
     @Override
     public Float getCount() {
-        a = new LinkedList<>();
-
-                return count;
+        return count;
     }
 
     @Override
